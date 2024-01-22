d1c8b3364d388902e7ebd9ad55cfd2aeaf10dec4
Heyner Rivas <heyner.rivas@globant.com>
2024-01-22 17:39:56 -0500

    Mensaje de tu commit

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -6,15 +6,41 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent {
+  operandoA : number;
+  operandoB : number;
+  
 
-  operandoA: number;
-  operandoB: number;
-
-  @Output() resultadoSuma = new EventEmitter<number>();
+@Output() resultadoSuma=new EventEmitter<number>;
 
   sumar():void{
    let resultado = this.operandoA + this.operandoB;
-   this.resultadoSuma.emit(resultado);
+   this.resultadoSuma.emit(resultado); 
+
   }
+
+  restar():void{
+    let resultado = this.operandoA - this.operandoB;
+    this.resultadoSuma.emit(resultado);
+ 
+   }
+
+   multiplicar():void{
+    let resultado = this.operandoA * this.operandoB;
+    this.resultadoSuma.emit(resultado);
+ 
+   }
+
+   dividir():void{
+    let resultado = this.operandoA / this.operandoB;
+    this.resultadoSuma.emit(resultado);
+ 
+   }
+
+   limpiarCampos() {
+    this.operandoA = 0;
+    this.operandoB = 0;
+  }
+
+
 
 }
